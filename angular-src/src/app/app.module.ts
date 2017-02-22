fbdcbe8e6a2b5c9dec7be928c1d8327b7a658a67
michaelwishlow <michael@pantheritconsulting.com>
2017-02-22 16:34:37 -0500

    Initial Router

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,6 +11,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+    {path:' ',  component: HomeComponent},
+    {path:'register ', component: RegisterComponent},
+    {path:'login ', component: LoginComponent},
+    {path:'dashboard', component: DashboardComponent},
+    {path:'profile ', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
@@ -24,7 +33,8 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
