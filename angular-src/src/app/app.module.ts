8c5a53669758057adfcd88bd7f9beaa001d7cbd5
michaelwishlow <michael@pantheritconsulting.com>
2017-02-23 12:48:39 -0500

    Routes Corrections

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,13 +12,14 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
-const appRoutes: Routes = [
-    {path:' ',  component: HomeComponent},
-    {path:'register ', component: RegisterComponent},
-    {path:'login ', component: LoginComponent},
-    {path:'dashboard', component: DashboardComponent},
-    {path:'profile ', component: ProfileComponent}
+ const appRoutes: Routes =  [
+ {path:'', component: HomeComponent},
+ {path:'register', component: RegisterComponent},
+ {path:'login', component: LoginComponent},
+ {path:'dashboard', component: DashboardComponent},
+ {path:'profile', component: ProfileComponent}
 ]
+
 
 @NgModule({
   declarations: [
