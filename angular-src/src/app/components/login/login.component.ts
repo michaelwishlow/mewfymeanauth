add41d4a91dcc9ccbf938090e96ba5f3b1342046
michaelwishlow <michael@pantheritconsulting.com>
2017-03-08 17:16:57 -0500

    Login Functions completed

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	username: String;
+	password: String;
 
-  constructor() { }
+  constructor(
+  		private authService:AuthService,
+  		private router:Router,
+  		private flashMessage:FlashMessagesService
+  		) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit(){
+  		const user = {
+  			username: this.username,
+  			password: this.password 
+  		}
+
+  		this.authService.authenticateUser(user).subscribe(data => {
+  				// console.log(data);  "Use as test before final code ~  Should produce proper True/False user data in console"
+  				if(data.success){
+  				  this.authService.storeUserData(data.token, data.user);
+  				  this.flashMessage.show('You Are Now Logged In', {
+  						cssClass: 'alert-success', 
+  						timeout:5000});
+  					this.router.navigate(['dashboard']);
+  				} else {
+  					this.flashMessage.show(data.msg, {
+  						cssClass: 'alert-danger', 
+  						timeout:5000});
+  					this.router.navigate(['login']);
+  				}
+  		});
+
+}
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -18,4 +18,20 @@ export class AuthService {
 
   }
 
+  authenticateUser(user){
+  		let headers = new Headers();
+  		headers.append('Content-Type','application/json');
+  		return this.http.post('http://localhost:3000/users/authenticate', user,{headers: headers})
+  			.map(res => res.json());
+
+  }
+
+  storeUserData(token, user){
+  		localStorage.setItem('id_token', token);  //specific path for angular jwt
+  		localStorage.setItem('user', JSON.stringify(user));  //local storage only holds strings not objects
+  		this.authToken = token;
+  		this.user = user;
+
+  }
+
 }
