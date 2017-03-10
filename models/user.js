dc36d2bf2d2b4826da028c12cb3cf24928008149
michaelwishlow <michael@pantheritconsulting.com>
2017-03-10 16:15:37 -0500

    Auth Corrections

--- models/user.js
+++ models/user.js
@@ -5,31 +5,31 @@ const config = require('../config/database');
 // User Schema
 const UserSchema = mongoose.Schema({
 	name: {
-		type: String,
+	type: String,
 	},
 	email: {
-		type: String,
-		required:  true
+	type: String,
+	required:  true
 	},
 	username: {
-		type: String,
-		required: true
+	type: String,
+	required: true
 	},
 	password: {
-		type: String,
-		required: true
+	type: String,
+	required: true
 	}
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.getUserbyId = function (id, callback){
-	User.findById(id, callback);
+module.exports.getUserById = function(id, callback){
+  User.findById(id, callback);
 }
 
 module.exports.getUserByUsername = function(username, callback){
-	  const query = {username: username}
-	  User.findOne(query, callback);
+  const query = {username: username}
+  User.findOne(query, callback);
 }
 
 module.exports.addUser = function(newUser, callback){
